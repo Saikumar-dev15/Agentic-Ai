a050c7905aeee4e6ad511eebb6c6bb39668265af
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-08-01 15:27:37 +0530

    Did some Exercise Work on Args & kwargs

--- Phase 1/Week 1/Arbitary Arguments.py
+++ Phase 1/Week 1/Arbitary Arguments.py
@@ -64,3 +64,69 @@ def largest_num(*args):
     
 #largest_num(2,5,7,3,8)
 
+
+def largest(*args):
+    Largest_num = args[0]
+    
+    for num in args:
+        if num > Largest_num:
+            Largest_num = num
+
+#    print("Largest Number: ", num)
+#print()
+    
+largest(3,5,7,8,9,2,10)
+
+
+
+def Maxnumber(*args):
+    Max_num = args[0]
+    Min_num = args[0]
+    
+    for i in args:
+        if i > Max_num:
+            Max_number = i
+         
+        if i < Min_num:
+            Min_num = i
+            
+    print("Largest Number: ", Max_number)
+    print("Smallest Number: ", Min_num)
+    
+print()
+Maxnumber(1,3,4,5,2,10,14)
+
+
+
+def profile(**kwargs):
+    for values in kwargs.values():
+        print(values)
+        
+profile(name = "Sai",
+        city = "Hyd",
+        email = "kurapatilakshmisaikumar@gmail.com")
+
+
+def Calculator(*args):
+    total = 0
+    Max_number = args[0]
+    Min_number = args[0]
+    
+    for num in args:
+        total += num
+        
+        if num in args:
+            num> Max_number
+            Max_number = num
+            
+        if num < Min_number:
+            Min_number =num
+            
+    average = total/len(args)
+    
+    print("Sum : ", total)
+    print("Average: ", average)
+    print("Largest num ", Max_number)
+    print("Smallest Num: ", Min_number)
+    
+Calculator(10,15,20,30)
