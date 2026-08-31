caa448849fb45a40413271ca08f569dbf829c909
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-08-31 19:13:35 +0530

    Composition concept in OOPS

---
+++ Phase 1/Week 1/Composition.py
@@ -0,0 +1,46 @@
+#Composition means building a class by putting objects of other classes inside it
+
+class Engine:
+    def start(self):
+        print("Engine Started")
+        
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+        
+    def start_car(self):
+        self.engine.start()
+        print("Car Started")
+        
+car = Car()
+car.start_car()    
+
+
+#Exercise
+
+class HelmetDetector:
+    def detect(self):
+        print("Detecting Helmet....")
+    
+class PlateOCR:
+    def read_plate(self):
+        print("Reading the Number Plate")
+        
+class Database:
+    def save(self):
+        print("Saving Violation")
+        
+class HelmetSystem:
+    def __init__(self):
+        self.detector = HelmetDetector()
+        self.ocr = PlateOCR()
+        self.database = Database()
+        
+    def process(self):
+        self.detector.detect()
+        self.ocr.read_plate()
+        self.database.save()
+        
+system = HelmetSystem()
+system.process()
+        
