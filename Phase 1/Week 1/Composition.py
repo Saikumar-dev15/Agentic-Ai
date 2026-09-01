e3aaa202520c46baf6b8fa020d31b5b1c69c9fcf
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-09-01 19:13:06 +0530

    Exercise on Composition concept

--- Phase 1/Week 1/Composition.py
+++ Phase 1/Week 1/Composition.py
@@ -13,7 +13,7 @@ class Car:
         print("Car Started")
         
 car = Car()
-car.start_car()    
+#car.start_car()    
 
 
 #Exercise
@@ -61,5 +61,69 @@ detector = HelmetDetector()
 ocr = PlateOCR()
 database = Database()
 
-system = HelmetSystem(detector, ocr, database)
-system.process()
+#system = HelmetSystem(detector, ocr, database)
+#system.process()
+
+
+#Exercise 
+
+class CPU:
+    def info(self):
+        print("CPU is Processing")
+        
+class RAM:
+    def performance(self):
+        print("RAM Loading......")
+        
+class Storage:
+    def Capacity(self):
+        print("Storage Storing Data....")
+
+class Computer:
+    def __init__(self,cpu, ram, storage):
+        self.cpu = cpu
+        self.ram = ram
+        self.storage = storage
+        
+    def run(self):
+        self.cpu.info()
+        self.ram.performance()
+        self.storage.Capacity()
+
+cpu = CPU()
+ram = RAM()
+storage = Storage()
+
+computer = Computer(cpu, ram, storage)
+#computer.run()
+
+
+
+
+#Exercise
+
+class Email:
+    def send(self, message):
+        print(f"Email Sent: {message}")
+        
+class SMS:
+    def send(self, message):
+        print(f"SMS Sent: {message}")
+        
+class NotificationSystem:
+    def __init__(self, notification):
+        self.notification = notification
+        
+    def notify(self, message):
+        self.notification.send(message)
+        
+email = Email()
+system = NotificationSystem(email)
+
+system.notify(input("Enter the Information: "))
+
+
+sms = SMS()
+system = NotificationSystem(sms)
+
+system.notify(input("Sent a msg: "))
