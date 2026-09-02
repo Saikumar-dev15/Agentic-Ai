cf8a748b590c87506d5768530cc291a214df94bc
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-09-02 22:34:58 +0530

    Completed Exercise on Dictionary and Bank withdrawal

--- Phase 1/Week 1/try,except,finally.py
+++ Phase 1/Week 1/try,except,finally.py
@@ -25,3 +25,53 @@ except ZeroDivisionError:
     
 finally:
     print("You are Accesed to see your Results!....")
+
+
+
+
+#Exercise 
+
+student = {
+    "name" : "Sai",
+    "Age"  : 19,
+    "course" : "AARS"
+}
+
+try:
+    key = input("Enter a Key: ")
+    value = student[key]
+    
+    print("Value: ", value)
+    
+except KeyError:
+    print("You Enter a Wrong Name Please verify...")
+    
+finally :
+    print("Program completed")
+
+
+
+
+#Exercise
+
+Balance = 5000
+
+try:
+    amount = int(input("Enter a amount: "))
+    if amount <= 0:
+        print("Amount Should be greater than 0")
+    
+    elif amount > Balance:
+        print("Invalid amount")
+        
+    else:
+       Balance -= amount
+       print("Withdrawed an amount : ", amount)
+       print("Remaining Balance: ", Balance)
+    
+except ValueError:
+    print("Invalid number. Suggested you to check Entered amount....")
+      
+finally:
+    print("Transaction Completed..")
+        
