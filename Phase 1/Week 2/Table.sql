ddf7f4199e2c247d0687b38fcad5d540bdc43ec0
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-09-06 13:00:57 +0530

    Intialize the values in the table and perform Manipulated Table

--- Phase 1/Week 2/Table.sql
+++ Phase 1/Week 2/Table.sql
@@ -20,5 +20,85 @@ CREATE TABLE  job_applied(
     status VARCHAR(50)
 );
  
+INSERT INTO job_applied(
+    job_id,
+    application_sent_data,
+    custom_resume,
+    resume_file_name,
+    cover_letter_sent,
+    cover_letter_file_name,
+    status)
+
+VALUES  (1,
+        '2026-09-05',
+        true,
+        'resume_01.pdf',
+        true,
+        'cover_letter_01.pdf',
+        'submitted'),
+        (2,
+        '2026-09-06',
+        false,
+        'resume_02.pdf',
+        false,
+        NULL,
+        'interview scheduled'),
+        (3,
+        '2026-09-07',
+        true,
+        'resume_03.pdf',
+        true,
+        'cover_letter_03.pdf',
+        'ghosted'),
+        (4,
+        '2026-09-07',
+        true,
+        'resume_04.pdf',
+        false,
+        NULL,
+        'submitted'),
+        (5,
+        '2026-09-08',
+        false,
+        'resume_05.pdf',
+        true,
+        'cover_letter_05.pdf',
+        'rejected');
+
+ALTER TABLE job_applied
+ADD contact VARCHAR(50);
+
+UPDATE job_applied 
+SET contact = 'Sai Kumar'
+WHERE job_id = 1;
+
+UPDATE job_applied 
+SET contact = 'Vaishnavi'
+WHERE job_id = 2;
+
+UPDATE job_applied 
+SET contact = 'Jaideep'
+WHERE job_id = 3;
+
+UPDATE job_applied 
+SET contact = 'Shiva'
+WHERE job_id = 4;
+
+UPDATE job_applied 
+SET contact = 'Gowtham'
+WHERE job_id = 5;
+
+
+ALTER TABLE job_applied
+RENAME COLUMN contact TO contact_name
+
+ALTER TABLE job_applied
+ALTER COLUMN contact_name TYPE TEXT;
+
+ALTER TABLE job_applied
+Drop COLUMN contact_name;
+
+DROP TABLE job_applied;
+
 SELECT *
 FROM job_applied;
