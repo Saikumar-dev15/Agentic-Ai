ba553dd017ff40b980d00b6b5a7376bddd0cd10a
Sai kumar <kurapatilakshmisaikumar@gmail.com>
2026-09-09 18:45:51 +0530

    Unions ALL and Exercise completed

--- Phase 1/Week 2/UNIONS.sql
+++ Phase 1/Week 2/UNIONS.sql
@@ -5,13 +5,57 @@ SELECT
      country
 FROM january_jobs
 
-UNION
+UNION ALL
 
 SELECT
     job_title,
     company_name,
     job_id,
     country
-FROM february_jobs;
+FROM february_jobs
+
+UNION ALL
+
+SELECT
+    job_title,
+    company_name,
+    job_id,
+    country
+FROM march_jobs;
+
+
+SELECT *
+FROM january_jobs; 
+
+-- Exercise 
+/*
+Find the job postings from the first quarter that have a salary greater than $60K
+- Combine job posting tables from the first quater of 2026 (Jan - Mar) 
+- Gets job postings with an average yearly salary > $60,000
+*/
+
+SELECT 
+     quarter1_job_postings.job_title,
+     quarter1_job_postings.country,
+     quarter1_job_postings.posted_date::DATE,
+     quarter1_job_postings.job_status,
+     quarter1_job_postings.salary_inr
+FROM (
+     SELECT *
+     FROM january_jobs
+     UNION ALL
+     SELECT *
+     FROM february_jobs
+     UNION ALL
+     SELECT *
+     FROM march_jobs
+) AS quarter1_job_postings
+WHERE 
+     quarter1_job_postings.salary_inr > 60000 AND
+     quarter1_job_postings.job_title = 'Data Analyst'
+ORDER BY
+     quarter1_job_postings.salary_inr
+
+
 
 
